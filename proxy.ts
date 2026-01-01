da2b6a2a8b8366b9f02c80a82968f0c5afd396ad
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2026-01-01 23:46:10 +0000

    fix proxy logic

--- proxy.ts
+++ proxy.ts
@@ -9,6 +9,7 @@ const publicRoutes = ['/sign-in', '/sign-up'];
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const cookieStore = await cookies();
+
   const accessToken = cookieStore.get('accessToken')?.value;
   const refreshToken = cookieStore.get('refreshToken')?.value;
 
@@ -20,59 +21,76 @@ export async function proxy(req: NextRequest) {
     pathname.startsWith(route)
   );
 
-  if (!accessToken) {
-    if (refreshToken) {
-      const data = await checkSessionServer();
-      const setCookie = data.headers['set-cookie'];
-
-      if (setCookie) {
-        const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
-
-        for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.path,
-            MaxAge: Number(parsed['Max-Age']),
-          };
-
-          if (parsed.accessToken)
-            cookieStore.set('accessToken', parsed.accessToken, options);
-
-          if (parsed.refreshToken)
-            cookieStore.set('refreshToken', parsed.refreshToken, options);
-        }
-
-        if (isPublicRoutes) {
-          return NextResponse.redirect(new URL('/', req.url), {
-            headers: { Cookie: cookieStore.toString() },
-          });
-        }
-
-        if (isPrivateRoutes) {
-          return NextResponse.next({
-            headers: { Cookie: cookieStore.toString() },
-          });
-        }
-      }
-    }
+  if (accessToken) {
     if (isPublicRoutes) {
-      return NextResponse.next();
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+    return NextResponse.next();
+  }
+
+  if (!accessToken && refreshToken) {
+    const data = await checkSessionServer();
+    const setCookie = data.headers['set-cookie'];
+
+    if (setCookie) {
+      const response = isPublicRoutes
+        ? NextResponse.redirect(new URL('/', req.url))
+        : NextResponse.next();
+
+      const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
+
+      for (const cookieStr of cookieArray) {
+        const parsed = parse(cookieStr);
+
+        const options = {
+          expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
+          path: parsed.path,
+          MaxAge: Number(parsed['Max-Age']),
+        };
+
+        if (parsed.accessToken)
+          cookieStore.set('accessToken', parsed.accessToken, options);
+
+        if (parsed.refreshToken)
+          cookieStore.set('refreshToken', parsed.refreshToken, options);
+      }
+
+      // if (isPublicRoutes) {
+      //   return NextResponse.redirect(new URL('/', req.url), {
+      //     headers: { Cookie: cookieStore.toString() },
+      //   });
+      // }
+
+      // if (isPrivateRoutes) {
+      //   return NextResponse.next({
+      //     headers: { Cookie: cookieStore.toString() },
+      //   });
+      // }
+      return response;
     }
 
     if (isPrivateRoutes) {
-      return NextResponse.redirect(new URL('/sign-in', req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
-  }
 
-  if (isPublicRoutes) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-
-  if (isPrivateRoutes) {
     return NextResponse.next();
+
+    // if (isPublicRoutes) {
+    //   return NextResponse.next();
+    // }
+
+    // if (isPrivateRoutes) {
+    //   return NextResponse.redirect(new URL('/sign-in', req.url));
+    // }
   }
+  return NextResponse.next();
+  // if (isPublicRoutes) {
+  //   return NextResponse.redirect(new URL('/', req.url));
+  // }
+
+  // if (isPrivateRoutes) {
+  //   return NextResponse.next();
+  // }
 }
 
 export const config = {
